227b690650c11ad9fdedbf3850ee9a49366c07c1
KATHLEEN CHEN <kc387@duke.edu>
2021-03-03 01:39:17 -0500

    add use cases

---
+++ doc/plan/usecases/changeTurtleImage.java
@@ -0,0 +1,7 @@
+// button is pressed by user to either upload a turtal image to use or choose from pre-esisting images
+
+// if images are preset, they would be stored in a css file
+
+// if the decision is made to upload images, image files can be sotred in a data folder to be accessed
+
+// screen displays new turtle image
