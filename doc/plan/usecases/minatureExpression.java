3e10eec2c61f7cce7221fa15c84257d0e9fa84cb
Casey Szilagyi <crs79@duke.edu>
2021-03-02 23:55:52 -0500

    Use cases

---
+++ doc/plan/usecases/minatureExpression.java
@@ -0,0 +1,15 @@
+/**
+ IF greater 2 1 [ fd 50]
+
+ Command passed from the view to the parser
+
+ The parser recognizes that the IF needs a single number
+
+ Recognizes that greater needs 2 arguments
+
+ The next two arguments are given to a greater command, which is executed and the result is given
+ to the if command which is executed
+
+ If the if command is nonzero, then the forward 50 is acted on the turtle
+
+ */
