cc5bd515360edbb769f92ea85386c379adfc2f54
Casey Szilagyi <crs79@duke.edu>
2021-03-04 00:17:20 -0500

    Made the class for tree parsing, began to think about the logistics

---
+++ src/slogo/model/commands/TreeParser.java
@@ -0,0 +1,27 @@
+package slogo.model.commands;
+
+import com.sun.source.tree.Tree;
+
+/**
+ * Once the input has been parsed into tree form, this class takes that tree and turns it into a
+ * functional command chain that will return TurtleMovement objects
+ */
+public class TreeParser {
+
+  CommandClassLoader COMMAND_CLASS_LOADER = new CommandClassLoader();
+  String TREE;
+
+  public TreeParser(String test){
+    TREE = test;
+  }
+
+
+  //for testing, still need to convert to tree form
+  public static void main(String[] args){
+    TreeParser testing = new TreeParser("fd 50");
+
+  }
+
+}
+
+
