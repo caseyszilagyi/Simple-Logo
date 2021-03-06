fb3528e81ea860f2eb9442df3d969c583dc8f816
Jin Cho <jc695@duke.edu>
2021-03-05 21:30:21 -0500

    adding properties files to define comman parameter ocunt

--- src/slogo/model/CommandParserTest.java
+++ src/slogo/model/CommandParserTest.java
@@ -1,12 +1,16 @@
 package slogo.model;
 
+import java.util.AbstractMap;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map.Entry;
+import java.util.ResourceBundle;
 import java.util.regex.Pattern;
 
 public class CommandParserTest implements Parser{
 
     // where to find resources specifically for this class
-    private static final String RESOURCES_PACKAGE = ProgramParser.class.getPackageName() + ".resources.languages.";
+    private static final String RESOURCES_PACKAGE = ".resources.languages.";
     // "types" and the regular expression patterns that recognize those types
     // note, it is a list because order matters (some patterns may be more generic)
     private List<Entry<String, Pattern>> mySymbols;
@@ -16,6 +20,19 @@ public class CommandParserTest implements Parser{
     @Override
     public void translateCommand(List<String> commandsBeforeTranslation) {
 
+    }
+
+    /**
+     * Adds the given resource file to this language's recognized types
+     */
+    public void addPatterns (String syntax) {
+        ResourceBundle resources = ResourceBundle.getBundle(RESOURCES_PACKAGE + syntax);
+        for (String key : Collections.list(resources.getKeys())) {
+            String regex = resources.getString(key);
+            mySymbols.add(new AbstractMap.SimpleEntry<>(key,
+                    // THIS IS THE IMPORTANT LINE
+                    Pattern.compile(regex, Pattern.CASE_INSENSITIVE)));
+        }
     }
 
     // Returns true if the given text matches the given regular expression pattern
