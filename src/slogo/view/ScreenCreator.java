6c24b0220bd6cf071fecf988bf00f4603659b2b2
KATHLEEN CHEN <kc387@duke.edu>
2021-03-06 22:25:55 -0500

    get turtle to move

--- src/slogo/view/ScreenCreator.java
+++ src/slogo/view/ScreenCreator.java
@@ -1,7 +1,12 @@
 package slogo.view;
 
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import slogo.controller.ViewController;
 
@@ -20,6 +25,7 @@ public class ScreenCreator {
   private UserCommandPane userCommand;
   private ViewPane viewPane;
   private ViewController viewController;
+  private VBox buttonBox;
 
   public ScreenCreator(ViewController viewController) {
     this.viewController = viewController;
@@ -40,5 +46,41 @@ public class ScreenCreator {
 
     viewPane = new ViewPane();
     root.setCenter(viewPane.getBox());
+
+    // TODO: remove later (testing)
+    // For testing for now
+    buttonBox = new VBox();
+    buttonBox.setAlignment(Pos.TOP_CENTER);
+    buttonBox.setSpacing(5.0);
+    root.setLeft(buttonBox);
+    addTitle();
+    createButtons();
+  }
+
+  // TODO: remove later (testing)
+  private void addTitle() {
+    Text title = new Text("Testing points");
+    buttonBox.getChildren().add(title);
+  }
+
+  // TODO: remove later (testing)
+  private void createButtons() {
+    Button centerButton = buttonCreation("(0.0, 0.0)");
+    centerButton.setOnAction(event -> viewPane.moveTurtle(0, 0));
+    Button planeI = buttonCreation("(3.0, 4.0)");
+    planeI.setOnAction(event -> viewPane.moveTurtle(3.0, 4.0));
+    Button planeII = buttonCreation("(-3.0, 4.0)");
+    planeII.setOnAction(event -> viewPane.moveTurtle(-3.0, 4.0));
+    Button planeIII = buttonCreation("(-3.0, -4.0)");
+    planeIII.setOnAction(event -> viewPane.moveTurtle(-3.0, -4.0));
+    Button planeIV = buttonCreation("(3.0, -4.0)");
+    planeIV.setOnAction(event -> viewPane.moveTurtle(3.0, -4.0));
+  }
+
+  // TODO: remove later (testing)
+  private Button buttonCreation(String text) {
+    Button button = new Button(text);
+    buttonBox.getChildren().add(button);
+    return button;
   }
 }
--- src/slogo/view/ViewPane.java
+++ src/slogo/view/ViewPane.java
@@ -1,9 +1,11 @@
 package slogo.view;
 
+import java.lang.Math;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
+import javafx.scene.layout.*;
 
 /**
  * Creates the view for where the turtle will be displayed
@@ -13,20 +15,59 @@ public class ViewPane {
   public static final double TURTLE_WIDTH = 70.0;
   public static final double TURTLE_HEIGHT = 70.0;
 
-  private HBox paneBox;
-  private turtleDisplay turtle;
+  public static final int rows = 21;
+  public static final int cols = 21;
+
+  private AnchorPane paneBox;
+  private ImageView turtle;
+
+  private double screenWidth;
+  private double screenHeight;
+  private double centerX;
+  private double centerY;
+
   public ViewPane() {
-    paneBox = new HBox();
-    paneBox.setAlignment(Pos.CENTER);
-    paneBox.setSpacing(5.0);
+    paneBox = new AnchorPane();
     // change once there is css file only used for testing
     paneBox.setStyle("-fx-padding: 10;" + "-fx-border-style: solid inside;"
             + "-fx-border-width: 2;" + "-fx-border-insets: 5;"
-            + "-fx-border-radius: 5;" + "-fx-border-color: red;");
-    turtle = new turtleDisplay(paneBox);
+            + "-fx-border-radius: 5;" + "-fx-border-color: purple;");
+    screenWidth = paneBox.getWidth();
+    screenHeight = paneBox.getHeight();
+    createTurtle();
   }
 
-  public HBox getBox() {
+  private void createTurtle() {
+    Image image = new Image(this.getClass().getClassLoader().getResourceAsStream(TURTLE_IMAGE));
+    turtle = new ImageView(image);
+    turtle.setFitWidth(TURTLE_WIDTH);
+    turtle.setFitHeight(TURTLE_HEIGHT);
+    paneBox.getChildren().add(turtle);
+  }
+
+  public void moveTurtle(double xCoordinate, double yCoordinate) {
+    double turtleCenterX = TURTLE_WIDTH / 2;
+    double turtleCenterY = TURTLE_HEIGHT / 2;
+
+    screenWidth = paneBox.getWidth();
+    screenHeight = paneBox.getHeight();
+
+    double coordinateWidth = screenWidth / cols;
+    double coordinateHeight = screenHeight / cols;
+
+    centerX = screenWidth / 2;
+    centerY = screenHeight / 2;
+
+    double x = centerX + xCoordinate * coordinateWidth - turtleCenterX;
+    double y = centerY - yCoordinate * coordinateHeight - turtleCenterY;
+
+    turtle.setX(x);
+    turtle.setY(y);
+  }
+
+  public AnchorPane getBox() {
     return paneBox;
   }
+
+
 }
