10af557a4635f8df6012a5eb2457a1c5cb1e01dd
Casey Szilagyi <caseyszilagyi@gmail.com>
2021-03-19 21:39:45 -0400

    Got basic version of tell command working, only lightly tested

--- src/slogo/model/commands/basic_commands/Tell.java
+++ src/slogo/model/commands/basic_commands/Tell.java
@@ -1,5 +1,6 @@
 package slogo.model.commands.basic_commands;
 
+import java.util.ArrayList;
 import java.util.List;
 import slogo.model.commands.basic_commands.command_types.MultipleTurtleCommand;
 import slogo.model.execution.CommandInformationBundle;
@@ -10,14 +11,27 @@ import slogo.model.tree.TreeNode;
  */
 public class Tell extends MultipleTurtleCommand {
 
+  private final List<Integer> IDS = new ArrayList<>();
+
+  /**
+   * Gets all of the turtles that will follow future actions
+   * @param informationBundle The bundle that contains all turtles
+   * @param children The integer values representing the turtles that will follow future commands
+   */
   public Tell(CommandInformationBundle informationBundle, List<TreeNode> children){
     super(informationBundle);
-
+    for(TreeNode child: children){
+      IDS.add((int) loadClass(informationBundle, child).execute());
+    }
   }
 
-
-
+  /**
+   *
+   * @return
+   */
+  @Override
   public double execute(){
-    return 0;
+    replaceActiveTurtleLayer(IDS);
+    return IDS.get(IDS.size()-1);
   }
 }
