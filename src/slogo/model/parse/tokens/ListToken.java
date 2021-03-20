4dfbcfb31ce2f8b482bc4f1ada21a13afc3de10c
Jin Cho <jc695@duke.edu>
2021-03-20 19:30:40 -0400

    command list can now take in non constants as param. need to test

--- src/slogo/model/parse/tokens/ListToken.java
+++ src/slogo/model/parse/tokens/ListToken.java
@@ -1,16 +1,30 @@
 package slogo.model.parse.tokens;
 
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+import java.util.List;
 import java.util.regex.Pattern;
 import slogo.model.parse.CommandParser;
 
 public abstract class ListToken extends Token{
 
   private int paramCount;
+  protected List<Token> params;
+  protected Deque<List<String>> paramTokensExpected;
 
 
   public ListToken(String command) {
     super("CommandBlock");
     paramCount = 0;
+    paramTokensExpected = new ArrayDeque<>();
+    params = new ArrayList<>();
+  }
+
+  @Override
+  public int incrementParamCount(int blockSize, Token command) {
+    addParamToken(command);
+    return params.size();
   }
 
 
