3aa4bfb08f321c9b3a694b5d8f34062751793a90
Jin Cho <jc695@duke.edu>
2021-03-21 18:19:08 -0400

    fixed refactoring command parser

--- src/slogo/model/parse/CommandParser.java
+++ src/slogo/model/parse/CommandParser.java
@@ -38,8 +38,6 @@ public class CommandParser extends Parser {
     commandBlockParser = new CommandBlockParser(cleanCommands, this);
     commandTree = new TreeNode(null, null);
     addUserDefParamCounts();
-    System.out.println("Command Taken in by the parser: " + rawInput);
-    System.out.println("Clean command: " + cleanCommands);
   }
 
   /**
@@ -54,17 +52,13 @@ public class CommandParser extends Parser {
 
   private void addUserDefParamCounts() {
     Map<String, UserDefinedCommand> userDefCommands = modelController.getUserDefinedCommands();
-    System.out.println(userDefCommands.keySet());
     for (String key : userDefCommands.keySet()) {
       int paramCounts = userDefCommands.get(key).getParamCount();
-      System.out.println("Command: "+key +" with numParam "+paramCounts);
-
       List<String> paramString = new ArrayList<>();
       for (int i = 0; i < paramCounts; i++) {
         paramString.add("NUM");
       }
       addSingleParamCount(key, paramString);
-      System.out.println(commandParam.get(key));
     }
   }
 
@@ -74,10 +68,7 @@ public class CommandParser extends Parser {
    * @return root node of command tree
    */
   public TreeNode makeTree() {
-    System.out.println(commandParam);
     Deque<Token> commandQueue = new LinkedList<>(cleanCommands);
-    System.out.println("QUEUE: " + commandQueue);
-
     while (!commandQueue.isEmpty()) {
       String command = commandQueue.removeFirst().getValue();
       TreeNode child = new TreeNode(command, null);
@@ -86,7 +77,6 @@ public class CommandParser extends Parser {
       insertNodeRecursive(commandQueue, child);
     }
     preOrderTraverse(commandTree);
-    System.out.println(preOrderResults);
     return commandTree;
   }
 
@@ -95,9 +85,7 @@ public class CommandParser extends Parser {
     if (root == null) {
       return;
     }
-    System.out.println("Value: " + root.getCommand());
     for (TreeNode child : root.getChildren()) {
-      System.out.println("Child to look at "+child.getValue());
       preOrderTraverse(child);
     }
   }
@@ -108,19 +96,15 @@ public class CommandParser extends Parser {
   }
 
   private TreeNode insertNodeRecursive(Deque<Token> splitCommands, TreeNode root) {
-    System.out.println("root val: "+root.getValue());
     if (getParam(root.getValue()).size() == 0) {
       System.out.println(root.getValue() + " is a leaf");
       return root;
     }
-    System.out.println();
     List<String> params = getParam(root.getValue());
     if(childOfMakeUserInstruction(root) && !root.getCommand().equals("CommandBlock")){
       params = new ArrayList<>();
     }
-    System.out.println("param count: "+params.size() +" for "+root.getValue());
     for (int i = 0; i < params.size(); i++) {
-      System.out.println("looking at "+i+"th param for "+root.getValue());
       String command;
       Token commandToken;
       try {
@@ -132,11 +116,8 @@ public class CommandParser extends Parser {
       TreeNode dummy = new TreeNode(command, root);
       dummy = checkCommandBlock(dummy);
       root.addChild(dummy);
-      System.out.println("Parent: " + root.getCommand());
-      System.out.println("Child: " + dummy.getCommand());
       insertNodeRecursive(splitCommands, dummy);
     }
-    System.out.println(root.getCommand());
     if (!correctChildren(root.getChildren(), params)) {
       throw new ErrorHandler("WrongParamNum");
     }
@@ -154,12 +135,11 @@ public class CommandParser extends Parser {
    * @param text String representation of the command
    * @return String rep of the number of params needed for command
    */
-  public List<String> getParam(String text) {
-    System.out.println(commandParam.containsKey(text));
-    try {
-      System.out.println("param: "+ commandParam.get(text).get(0));
-      return commandParam.get(text);
-    } catch (Exception e) {
+  private List<String> getParam(String text) {
+    List<String> paramList = commandParam.get(text);
+    if (paramList != null) {
+      return paramList;
+    } else {
       return new ArrayList<>();
     }
   }
@@ -172,8 +152,6 @@ public class CommandParser extends Parser {
   }
 
   private boolean correctChildren(List<TreeNode> children, List<String> expected) {
-    System.out.println(children.isEmpty());
-    System.out.println(expected);
     if(children.isEmpty() && expected==null) {  return true; }
     if(expected==null || children.size() != expected.size()) {  return false; }
     List<String> toMatch = new ArrayList<>();
@@ -182,8 +160,6 @@ public class CommandParser extends Parser {
       if(match(t.getCommand(), syntaxMap.get("Command"), syntaxMap.get("Variable"), syntaxMap.get("Constant"))) { toMatch.add("NUM"); }
       if(t.getCommand().equals("CommandBlock")) { toMatch.set(ind, "LIST");}
     }
-    System.out.println(toMatch);
-    System.out.println(expected);
     return toMatch.equals(expected);
   }
 
